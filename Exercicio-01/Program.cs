359a0c9c69f8fd445912fd9da9e6a9772b44c974
Abdon192 <junior_smj@outlook.com>
2022-03-02 22:25:20 -0300

    commit exercicio 1

---
+++ Exercicio-01/Program.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Exercicio_01
+{
+    internal class Program
+    {
+        static void Main(string[] args){
+            Console.WriteLine("Informe um valor");
+            
+            decimal real,dolar,resultado;
+            decimal.TryParse(Console.ReadLine(),out real);
+            dolar = 0.1932m;
+            resultado = real * dolar;
+
+            Console.WriteLine("Resultado:"+resultado.ToString());
+        }
+    }
+    
+}
